87fabc83f3c7fcffa0dd44f96b0921134ee75390
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-11-13 12:56:19 +0100

    test

--- go/base/install-go.sh
+++ go/base/install-go.sh
@@ -2,7 +2,7 @@
 # This script install the Go version correct for each architecture.
 set -e
 
-GOLANG_VERSION=1.21.4
+GOLANG_VERSION=1.21.3
 GOLANG_DOWNLOAD_URL=https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz
 GOLANG_DOWNLOAD_SHA256_AMD=73cac0215254d0c7d1241fa40837851f3b9a8a742d0b54714cbdfb3feaf8f0af
 GOLANG_DOWNLOAD_SHA256_ARM=ce1983a7289856c3a918e1fd26d41e072cc39f928adfb11ba1896440849b95da
